b21fc9ec5489a605e50325e2464486523f92bd4f
KB <katrinkajb@gmail.com>
2021-06-01 11:40:59 -0700

    got auth pages working and started tests

--- bookings-web/src/containers/Getaways.test.jsx
+++ bookings-web/src/containers/Getaways.test.jsx
@@ -5,5 +5,7 @@ import Getaways from './Getaways';
 describe('Getaways test', async () => {
     it('renders the app to the page', () => {
         render(<Getaways />)
+
+        const ul = await screen.findByRole('list', {name: 'places'});
     })
 })
